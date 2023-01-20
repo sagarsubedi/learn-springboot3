298a9f7b4d1ba2a0553398936a4896ed07a1673c
Sagar Subedi <sagar@example.com>
2023-01-20 16:43:52 -0600

    database up and running

--- src/main/java/com/sagar/Main.java
+++ src/main/java/com/sagar/Main.java
@@ -20,22 +20,22 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-//    abbvr for @RequestMapping and method=RequestMethod.GET property
-    public GreetResponse greet() {
-        return new GreetResponse("hello", List.of("JavaScript", "Java"), new Person("Sagar", 22, 100));
-    }
-
-    // record GreetResponse(String greet){}
-    // record is same as having GreetResponse: final greet, getter, hash, equals (everything is final so no setter)
-
-    // each param is key for its own json object
-    record Person(String name, int age, double cashOnHand){}
-
-    // every param is a key for the json object
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ){}
+//    @GetMapping("/")
+////    abbvr for @RequestMapping and method=RequestMethod.GET property
+//    public GreetResponse greet() {
+//        return new GreetResponse("hello", List.of("JavaScript", "Java"), new Person("Sagar", 22, 100));
+//    }
+//
+//    // record GreetResponse(String greet){}
+//    // record is same as having GreetResponse: final greet, getter, hash, equals (everything is final so no setter)
+//
+//    // each param is key for its own json object
+//    record Person(String name, int age, double cashOnHand){}
+//
+//    // every param is a key for the json object
+//    record GreetResponse(
+//            String greet,
+//            List<String> favProgrammingLanguages,
+//            Person person
+//    ){}
 }
